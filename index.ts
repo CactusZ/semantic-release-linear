7242921fc3eb24f790c509c1a16997ed0cddd70c
Igor Kasevich <igorkasevich@gmail.com>
2022-07-12 16:13:23 +0200

    add publish step

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { analyzeCommits } from "./src/analyzeCommits";
 import { generateNotes } from "./src/generateNotes";
+import { publish } from "./src/publish";
 import { verifyConditions } from "./src/verifyConditions";
 
-export { verifyConditions, analyzeCommits, generateNotes };
+export { verifyConditions, analyzeCommits, generateNotes, publish };
